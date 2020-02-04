35f29c68f9f14a18e614666b07ead199bdedcacf
Maksym Maksak <maksak.dev@gmail.com>
2020-02-04 15:09:04 +0200

    lesson09 | done

---
+++ lesson09/script.js
@@ -0,0 +1,45 @@
+const start = document.getElementById('start'),
+    plusIncome = document.getElementsByTagName('button')[0],
+    plusExpenses = document.getElementsByTagName('button')[1],
+    additionalIncome = document.querySelectorAll('.additional_income-item'),
+    budgetMonthValue = document.querySelector('.budget_month-value'),
+    budgetDayValue = document.querySelector('.budget_day-value'),
+    expensesMonthValue = document.querySelector('.expenses_month-value'),
+    addIncomeValue = document.querySelector('.additional_income-value'),
+    addExpensesValue = document.querySelector('.additional_expenses-value'),
+    incomePeriodValue = document.querySelector('.income_period-value'),
+    targetMonthValue = document.querySelector('.target_month-value'),
+    salary = document.querySelector('.salary-amount'),
+    incomeTitle = document.querySelector('.income-items').querySelector('.income-title'),
+    incomeAmount = document.querySelector('.income-amount'),
+    expensesTitle = document.querySelector('.expenses-items').querySelector('.expenses-title'),
+    expensesAmount = document.querySelector('.expenses-amount'),
+    addExpensesItem = document.querySelector('.additional_expenses-item'),
+    target = document.querySelector('.target-amount'),
+    period = document.querySelector('.period-select'),
+    periodAmount = document.querySelector('.period-amount'),
+    depositAmount = document.querySelector('.deposit-amount'),
+    depositBank = document.querySelector('.deposit-bank'),
+    depositPercent = document.querySelector('.deposit-percent');
+    console.log(incomeTitle);
+
+console.log(additionalIncome);
+
+
+
+// Кнопку "Рассчитать" через id
+
+// Кнопки “+” (плюс) через Tag, каждую в своей переменной. 
+
+// Чекбокс по id через querySelector
+
+// Поля для ввода возможных доходов (additional_income-item) при помощи querySelectorAll
+
+// Каждый элемент в правой части программы через класс, которые имеют в имени класса "-value", начиная с class="budget_day-value" и заканчивая class="target_month-value">
+
+// Оставшиеся поля через querySelector каждый в отдельную переменную:
+
+// поля ввода (input) с левой стороны и не забудьте про range.
+
+
+
